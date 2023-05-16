576b43a0da28cd50f029d6e80473b851e14d8c67
elifyerlikaya <elifyerlikaya05@gmail.com>
2023-05-17 02:46:54 +0300

    iletişim düzenlendi

--- iletişim/iletişim.js
+++ iletişim/iletişim.js
@@ -1,18 +1,63 @@
-document.getElementById("contact-form").addEventListener("submit", function(event) {
-    event.preventDefault(); // Sayfanın yeniden yüklenmesini engeller
+const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
 
-    // Formdaki değerleri al
-    var name = document.getElementById("name").value;
-    var email = document.getElementById("email").value;
-    var phone = document.getElementById("phone").value;
-    var message = document.getElementById("message").value;
+function switchTheme(e) {
+    if (e.target.checked) {
+        document.documentElement.setAttribute('data-theme', 'dark');
+    }
+    else {
+        document.documentElement.setAttribute('data-theme', 'light');
+    }    
+}
 
-    // Form verilerini kullanarak işlemleri yapabilirsiniz
-    // Örneğin, verileri bir API'ye gönderebilir veya başka bir işlem yapabilirsiniz
+toggleSwitch.addEventListener('change', switchTheme, false);
 
-    // Formu sıfırla
-    document.getElementById("contact-form").reset();
 
-    // Kullanıcıya bir mesaj göster
-    alert("Mesajınız gönderildi! Teşekkür ederiz.");
-});
+const name = document.getElementById('name');
+const email = document.getElementById('email');
+const message = document.getElementById('message');
+const contactForm = document.getElementById('contact-form');
+const errorElement = document.getElementById('error');
+const successMsg = document.getElementById('success-msg');
+const submitBtn = document.getElementById('submit');
+  
+const validate = (e) => {
+  e.preventDefault();
+ 
+  if (name.value.length < 3) {
+    errorElement.innerHTML = 'Your name should be at least 3 characters long.';
+    return false;
+  } 
+  
+  if (!(email.value.includes('.') && (email.value.includes('@')))) {
+    errorElement.innerHTML = 'Please enter a valid email address.';
+    return false;
+  } 
+
+  if (!emailIsValid(email.value)) {
+    errorElement.innerHTML = 'Please enter a valid email address.';
+    return false;
+  }
+
+  if (message.value.length < 15) {
+    errorElement.innerHTML = 'Please write a longer message.';
+    return false;
+  }
+
+  errorElement.innerHTML = '';
+  successMsg.innerHTML = 'Thank you! I will get back to you as soon as possible.'; 
+
+  e.preventDefault();
+  setTimeout(function () {
+    successMsg.innerHTML = '';
+    document.getElementById('contact-form').reset();
+  }, 6000);
+
+  return true;
+
+}
+
+const emailIsValid = email => {
+  return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+}
+
+submitBtn.addEventListener('click', validate);
