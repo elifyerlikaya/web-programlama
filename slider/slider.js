0f7adff0bad9b8e71d6f91c4cf3b64a4d0eecf4a
elifyerlikaya <elifyerlikaya05@gmail.com>
2023-05-16 23:59:05 +0300

    şehrim slider yapıldı

---
+++ slider/slider.js
@@ -0,0 +1,45 @@
+function yesno() {
+
+    var checkboxvar = document.getElementById("tıkla");
+    var labelvar = document.getElementById("text");
+    if (!checkboxvar.checked) {
+        labelvar.innerHTML = "";
+    }
+    else {
+        labelvar.innerHTML = "Saklıkent";
+    }
+  }
+  function yesnox() {
+  
+    var checkboxvar = document.getElementById("tıklax");
+    var labelvar = document.getElementById("textx");
+    if (!checkboxvar.checked) {
+        labelvar.innerHTML = "";
+    }
+    else {
+        labelvar.innerHTML = "Marmaris";
+    }
+  }
+  function yesnoy() {
+  
+    var checkboxvar = document.getElementById("tıklay");
+     var labelvar = document.getElementById("texty");
+    if (!checkboxvar.checked) {
+        labelvar.innerHTML = "";
+    }
+    else {
+        labelvar.innerHTML = "Ölüdeniz";
+    }
+  } 
+  
+  function yesnoz() {
+  
+    var checkboxvar = document.getElementById("tıklaz");
+    var labelvar = document.getElementById("textz");
+    if (!checkboxvar.checked) {
+        labelvar.innerHTML = "";
+    }
+    else {
+        labelvar.innerHTML = "Fethiye";
+    }
+  }
